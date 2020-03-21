ae992fba750c7867a733728a4c474b5296f379da
KaO <wasurete@gmail.com>
2020-03-21 16:27:02 -0700

    Initial Unity Project

---
+++ Assets/Hazardous.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazardous: MonoBehaviour 
+{
+   	void OnCollisionEnter(Collision collision)
+	{
+		 if (collision.gameObject.tag == "Player") 
+		{ 
+	          GameManager.isGameOver = true; 
+	     } 
+   } 
+}
+
+
+
+
